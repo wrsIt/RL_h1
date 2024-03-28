0adabc4597736d4830b71b393556df6f10ab0f11
lemon <2544342950@QQ.com>
2024-03-28 21:40:23 +0800

    写了一部分

--- pendulum_QLearning.py
+++ pendulum_QLearning.py
@@ -1,3 +1,4 @@
+import math
 import os
 
 import numpy as np
@@ -20,6 +21,8 @@ class Agent:
 
         self.actions = np.linspace(-self.max_action, self.max_action, self.action_n)  # 动作空间 -3 0 3
         self.env = PendulumEnv()  # 交互环境
+        self.THETA_STATES = np.round(np.linspace(-math.pi, math.pi, self.theta_n), decimals=4)
+        self.D_THETA_STATES = np.round(np.linspace(-15 * math.pi, 15 * math.pi, self.d_theta_n), decimals=4)
 
         self.q_table = np.zeros((self.theta_n, self.d_theta_n, len(self.actions)))
         # 初始化Q(state, action)表，其中state=(theta, d_theta)
@@ -48,21 +51,78 @@ class Agent:
         # print(table)
         return table
 
-    def update_env(self, observation, episode, step_counter):
-        if is_final(observation):
+    def is_final_state(self, state):
+        # final state:将摆杆摆起并稳定 state:(0,0) 状态区间是对称的，所以稳定状态就是当角度、角速度分别处于各自状态区间的中间位置时。
+        theta = state[0]
+        d_theta = state[1]
+        return self.is_mid_state(theta, self.THETA_STATES) and self.is_mid_state(d_theta, self.D_THETA_STATES)
 
-    def train(self, env, ):
-        q_table = build_q_table()
-        for episode in range(self.MAX_EPISODES):
-            step_counter = 0
-            # 与环境交互
-            observation, _ = self.env.reset()
-            observation = tuple(observation)
+    def is_mid_state(self, t, states):
+        # 根据给定的状态列表，判断给定的值是否处于这个状态列表的中间位置
+        idx_mid = len(states) // 2
+        if len(states) % 2 == 0:
+            right = states[idx_mid]
+            left = states[idx_mid - 1]
+            return left < t < right
+        else:
+            mid = states[idx_mid]
+            return t == mid
 
-            # 单个回合训练
-            is_terminated = False  # 标记该回合是否结束
-            update_env(observation, episode, step_counter)  # 更新环境
-            while step_counter < self.one_turn_step and (not is_terminated):
-                # 离散当前状态 以便在q表中索引
-                observation_round = round_observation(observation)
-                max_Q_action = choose_max_Q_action(observation_round, self.q_table)
+    def discretize_mapping(self, theta, states):
+        # theta是已经映射到[-pi,pi]的角度
+        # 查找theta在列表中该在哪个分组，返回离散化后对应的数值
+        idx = np.digitize(theta, bins=states)
+        if 0 < idx < len(states):
+            left = states[idx - 1]
+            right = states[idx]
+            # 看哪边更接近theta
+            if np.abs(left - theta) < np.abs(right - theta):
+                idx = idx - 1  # 左边更接近
+            else:
+                idx = idx
+        elif idx <= 0:
+            idx = 0
+        else:
+            idx = len(states) - 1
+        return states[idx]
+
+    def discretize_state(self, state):
+        # 离散化状态 切分区间->区间对应的状态数据
+        theta = state[0]
+        d_theta = state[1]
+        theta_copy = theta
+        # 角度映射到[-pi,pi] fmod是取余，参数是2pi，由于fmod函数的特性导致其范围是[0,2pi]，最后减去pi来平移区间。
+        x_ = np.fmod(theta_copy + np.pi, 2 * np.pi) - np.pi
+        theta = self.discretize_mapping(theta, self.THETA_STATES)
+        d_theta = self.discretize_mapping(d_theta, self.D_THETA_STATES)
+        return theta, d_theta  # 单位是弧度
+
+    def update_env(self, state, episode, step_counter):
+        if self.is_final_state(state):
+            content = "Episode %s : total step = %s" % (episode + 1, step_counter)
+            print('\n', content)
+        else:
+            state = self.discretize_state(state)  # 离散化状态 弧度
+            content = "Episode %s：step = %s" % (episode + 1, step_counter)
+            content += " angle：" + str(state[0] * 180 / math.pi) + ", angular_velocity：" + str(
+                state[1] / math.pi) + "pi"
+            print('\n', content)
+
+    def choose_max_Q_action(self, state, q_table):
+        state_actions = q_table.loc[state]  # 这个状态对应的一行
+
+
+        def train(self, env, is_view=False):
+            q_table = build_q_table()
+            for episode in range(self.MAX_EPISODES):
+                step_counter = 0
+                # 与环境交互
+                observation, _ = env.reset(is_view=is_view)
+                observation = tuple(observation)
+                # 单个回合训练
+                is_terminated = False  # 标记该回合是否结束
+                self.update_env(observation, episode, step_counter)  # 更新环境
+                while step_counter < self.one_turn_step and (not is_terminated):
+                    # 离散当前状态 以便在q表中索引
+                    observation_discretized = self.discretize_state(observation)
+                    max_Q_action = self.choose_max_Q_action(observation_discretized, self.q_table)
